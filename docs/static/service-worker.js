d2e4dfa67b0cac4f911dfbd399eb35734ff268ce
wangjue <wangjue@startdt.com>
2018-05-04 14:55:13 +0800

    sad

---
+++ docs/static/service-worker.js
@@ -0,0 +1,46 @@
+var cacheName = "douban";
+
+var filesToCache = [
+  "/",
+  "/index.html",
+  "/src/main.js",
+  "/styles/logo.css",
+  "/images/search.png",
+  "/images/star.png"
+];
+
+self.addEventListener("install", function(e) {
+  console.log("[ServiceWorker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log("[ServiceWorker] Caching app shell");
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", function(e) {
+  console.log("[ServiceWorker] Activate");
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(
+        keyList.map(function(key) {
+          if (key !== cacheName) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", function(e) {
+  console.log("[ServiceWorker] Fetch", e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
